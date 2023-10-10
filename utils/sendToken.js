16c95acabff032f5f3f43d619e65140e375e8c11
vidit <viditsharma818@gmail.com>
2023-10-10 08:19:40 +0530

    secure  hata diya

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -4,7 +4,7 @@ exports.sendtoken=(user,statuscode,res)=>{
     const options={
         expires:new Date(Date.now()+ process.env.COOKIE_EXPIRE *24 *60 * 60* 1000 ),
         httpOnly:true,
-        secure: true,
+        // secure: true,
         sameSite: 'none',
     }
     res.status(statuscode).cookie("token",token,options).json({
